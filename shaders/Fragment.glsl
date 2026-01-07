d9fd7a6647b3013bb6a5f1bf4a42745faf5c125c
TFlannery25 <timothy@timothyflannery.com>
2026-01-07 02:37:09 -0800

    Perspective and Lighting implemented

--- shaders/Fragment.glsl
+++ shaders/Fragment.glsl
@@ -1,8 +1,28 @@
-#version 330 core
+#version 410 core
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec3 vColor;
+
+//uniform vec3 viewPos;
+
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+uniform float lightInt;
 
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(0.3, 0.0, 1.0, 1.0); // color
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    //vec3 viewDir  = normalize(viewPos - FragPos);
+
+    vec3 ambient = vec3(0.4);
+
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor * lightInt;
+
+    vec3 col = (ambient + diffuse) * vColor;
+    FragColor = vec4(col, 1.0); // color
 }
